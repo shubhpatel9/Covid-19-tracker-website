ef4b93228290d5d7bd5946a5cf0efe78e49a7e20
Sarsaparilla29 <jacobthebennett@gmail.com>
2020-11-25 03:41:11 -0500

    fixed title

--- React/app-name/src/App.js
+++ React/app-name/src/App.js
@@ -6,10 +6,10 @@ import Counter from "./Components/Counter/Counter";
 function App() {
   return (
     <div>
-      <h1>Fuck, this is gonna be hard</h1>
+      <h1>COVID19 Data</h1>
       <Counter name={"Jacob"} counterValue={1}/>
       <Counter name={"Luke"} counterValue={10}/>
-      <Counter name={"Shubham"} counterValue={-9}/>
+      <Counter name={"Shubh"} counterValue={-9}/>
     </div>
   );
 }
